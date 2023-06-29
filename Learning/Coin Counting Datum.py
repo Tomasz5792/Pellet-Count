cda36dd9678b9200fa6c1d26d7d83a3fb93d3ce2
Tomasz <72576690+Tomasz5792@users.noreply.github.com>
2023-06-29 22:21:43 +0000

    Trying to set up environment to start learning opencv

---
+++ Learning/Coin Counting Datum.py
@@ -0,0 +1,9 @@
+#https://www.youtube.com/watch?v=XZ3PNnA9NbU
+
+import numpy as np
+import cv2
+
+#cant seem to get open cv to work
+print("Your OpenCV version is: " + cv2.__version__)
+
+#img = cv2.imread("5 coins.jfif")
